b7d7f154a7dfa6c9037dcf8922803ce56c1e4b93
Dariusz Okoński <darek200180@gmail.com>
2025-10-01 20:02:08 +0200

    add CreateProductDTO class

--- src/products/dtos/create-product.dto.ts
+++ src/products/dtos/create-product.dto.ts
@@ -1,6 +1,18 @@
 /* eslint-disable prettier/prettier */
-export type CreateProductDTO = {
+import { Transform } from 'class-transformer';
+import { IsInt, IsNotEmpty, IsString, Length, Min } from 'class-validator';
+
+export class CreateProductDTO {
+  @IsNotEmpty()
+  @IsString()
+  @Length(10, 20)
   name: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(0)
   price: number;
+
+  @Transform(({ value }) => (Array.isArray(value) ? value.join(', ') : ''))
   description: string;
-};
+}
