051df0818e60b2d0bfa819e1892ed82a11bf8081
Dariusz Okoński <darek200180@gmail.com>
2025-10-02 07:19:15 +0200

    update gitById order

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,5 +1,11 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
 
 @Controller('orders')
@@ -12,7 +18,13 @@ export class OrdersController {
   }
 
   @Get('/:id')
-  getById(@Param('id') id: string) {
+  getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const order = this.ordersService.getById(id);
+
+    if (!order) {
+      throw new NotFoundException('Order not found');
+    }
+
     return this.ordersService.getById(id);
   }
 }
