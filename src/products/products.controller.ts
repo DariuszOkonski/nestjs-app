120e3b2447901477dffddc050c5a2920426620f7
Dariusz Okoński <darek200180@gmail.com>
2025-10-03 07:09:30 +0200

    update products.service.ts to use db

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import {
   Body,
   Controller,
@@ -23,8 +24,8 @@ export class ProductsController {
   }
 
   @Get('/:id')
-  public getById(@Param('id', new ParseUUIDPipe()) id: string) {
-    const prod = this.productsService.getById(id);
+  public async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const prod = await this.productsService.getById(id);
 
     if (!prod) {
       throw new NotFoundException('Product not found');
@@ -34,12 +35,12 @@ export class ProductsController {
   }
 
   @Delete('/:id')
-  public deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
-    if (!this.productsService.getById(id)) {
+  public async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (await !this.productsService.getById(id)) {
       throw new NotFoundException('Product not found');
     }
 
-    this.productsService.deleteById(id);
+    await this.productsService.deleteById(id);
 
     return { success: true };
   }
@@ -50,15 +51,15 @@ export class ProductsController {
   }
 
   @Put('/:id')
-  public update(
+  public async update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() productData: UpdateProductDTO,
   ) {
-    if (!this.productsService.getById(id)) {
+    if (await !this.productsService.getById(id)) {
       throw new NotFoundException('Product not found');
     }
 
-    this.productsService.updateById(id, productData);
+    await this.productsService.updateById(id, productData);
     return { success: true };
   }
 }
